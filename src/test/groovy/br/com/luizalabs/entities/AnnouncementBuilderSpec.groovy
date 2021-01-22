4e6a567a42d4b27045d29929dfeb2e0c9c2aa738
Eduardo Custodio <du.yujixd@gmail.com>
2021-01-22 01:21:28 -0300

    Create de Announcement Builder spec

---
+++ src/test/groovy/br/com/luizalabs/entities/AnnouncementBuilderSpec.groovy
@@ -0,0 +1,94 @@
+package br.com.luizalabs.entities
+
+import spock.lang.Specification
+
+class AnnouncementBuilderSpec extends Specification {
+
+    def 'build a valid SMS Announcement'() {
+        def optionalAnnouncement = Announcement.builder()
+                .type(AnnouncementType.SMS)
+                .destinatary("11994196292")
+                .message("A test message")
+                .build();
+
+        expect:
+        optionalAnnouncement.isPresent()
+    }
+
+    def 'build a invalid SMS Announcement'() {
+        def optionalAnnouncement = Announcement.builder()
+                .type(AnnouncementType.SMS)
+                .destinatary("994196292")
+                .message("A test message")
+                .build();
+
+        expect:
+        !optionalAnnouncement.isPresent()
+    }
+
+    def 'build a valid email Announcement'() {
+        def optionalAnnouncement = Announcement.builder()
+                .type(AnnouncementType.EMAIL)
+                .destinatary("eduardo@gmail.com")
+                .message("A test message")
+                .build();
+
+        expect:
+        optionalAnnouncement.isPresent()
+    }
+
+    def 'build a invalid email Announcement'() {
+        def optionalAnnouncement = Announcement.builder()
+                .type(AnnouncementType.EMAIL)
+                .destinatary(".eduardo@gmail.com")
+                .message("A test message")
+                .build();
+
+        expect:
+        !optionalAnnouncement.isPresent()
+    }
+
+    def 'build a valid Push Announcement'() {
+        def optionalAnnouncement = Announcement.builder()
+                .type(AnnouncementType.PUSH)
+                .destinatary("1")
+                .message("A test message")
+                .build();
+
+        expect:
+        optionalAnnouncement.isPresent()
+    }
+
+    def 'build a invalid Push Announcement'() {
+        def optionalAnnouncement = Announcement.builder()
+                .type(AnnouncementType.PUSH)
+                .destinatary("")
+                .message("A test message")
+                .build();
+
+        expect:
+        !optionalAnnouncement.isPresent()
+    }
+
+    def 'build a valid WhatsApp Announcement'() {
+        def optionalAnnouncement = Announcement.builder()
+                .type(AnnouncementType.WHATSAPP)
+                .destinatary("11994196292")
+                .message("A test message")
+                .build();
+
+        expect:
+        optionalAnnouncement.isPresent()
+    }
+
+    def 'build a invalid WhatsApp Announcement'() {
+        def optionalAnnouncement = Announcement.builder()
+                .type(AnnouncementType.WHATSAPP)
+                .destinatary("994196292")
+                .message("A test message")
+                .build();
+
+        expect:
+        !optionalAnnouncement.isPresent()
+    }
+}
