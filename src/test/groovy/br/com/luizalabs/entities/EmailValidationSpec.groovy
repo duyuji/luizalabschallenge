1d9fe113ee21047445a703f5695c56f0ce7a55b9
Eduardo Custodio <du.yujixd@gmail.com>
2021-01-22 00:25:40 -0300

    Simplify the groovy code

--- src/test/groovy/br/com/luizalabs/entities/EmailValidationSpec.groovy
+++ src/test/groovy/br/com/luizalabs/entities/EmailValidationSpec.groovy
@@ -4,48 +4,48 @@ import spock.lang.Specification
 
 class EmailValidationSpec extends Specification {
 
-    def EMAIL_VALIDATOR = new EmailValidator();
+    def EMAIL_VALIDATOR = new EmailValidator()
 
     void 'test a valid email'() {
         def email = 'eduardocustodio@gmail.com'
 
         expect:
-        true == EMAIL_VALIDATOR.isValid(email)
+        EMAIL_VALIDATOR.isValid(email)
     }
 
     void 'test a valid email with .'() {
         def email = 'eduardo.custodio@gmail.com'
 
         expect:
-        true == EMAIL_VALIDATOR.isValid(email)
+        EMAIL_VALIDATOR.isValid(email)
     }
 
     void 'test a valid email with _'() {
         def email = 'eduardo_custodio@gmail.com'
 
         expect:
-        true == EMAIL_VALIDATOR.isValid(email)
+        EMAIL_VALIDATOR.isValid(email)
     }
 
     void 'test a valid email with _ at the beginning'() {
         def email = '_custodio@gmail.com'
 
         expect:
-        true == EMAIL_VALIDATOR.isValid(email)
+        EMAIL_VALIDATOR.isValid(email)
     }
 
     void 'test a invalid email with . at the beginning'() {
         def email = '.custodio@gmail.com'
 
         expect:
-        false == EMAIL_VALIDATOR.isValid(email)
+        !EMAIL_VALIDATOR.isValid(email)
     }
 
     void 'test a invalid email with @ at the beginning'() {
         def email = '@custodio@gmail.com'
 
         expect:
-        false == EMAIL_VALIDATOR.isValid(email)
+        !EMAIL_VALIDATOR.isValid(email)
     }
 
 
--- src/test/groovy/br/com/luizalabs/entities/PushIDSpecification.groovy
+++ src/test/groovy/br/com/luizalabs/entities/PushIDSpecification.groovy
@@ -3,19 +3,19 @@ package br.com.luizalabs.entities
 import spock.lang.Specification
 
 class PushIDSpecification extends Specification {
-    def PUSH_ID_VALIDATOR = new PushIDValidator();
+    def PUSH_ID_VALIDATOR = new PushIDValidator()
 
     def 'test a valid push id'() {
         def pushID = "1"
 
         expect:
-        true == PUSH_ID_VALIDATOR.isValid(pushID);
+        PUSH_ID_VALIDATOR.isValid(pushID)
     }
 
     def 'test a invalid push id'() {
         def pushID = ""
 
         expect:
-        false == PUSH_ID_VALIDATOR.isValid(pushID);
+        !PUSH_ID_VALIDATOR.isValid(pushID)
     }
 }
